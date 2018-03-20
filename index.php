1899dc0a645299fb32226292de7823a1257160af
pr11 <pr11@figd-1811711.figdtk.de>
2018-03-20 15:17:57 +0100

    new folder

--- index.php
+++ index.php
@@ -1,8 +1,52 @@
+<?php
+$src = 'source/3.jpg';
+$imgInfo = getimagesize($src);
+$srcWidth = $imgInfo[0];
+$srcHeight = $imgInfo[1];
+
+$srcImg = imagecreatefromjpeg($src);
+
+// gleiche Breite (alle Images)
+/*
+ *  2100 Höhe
+ *  2800 Breite
+ * 
+ *  2800   $dstWidth (200)
+ *  ---- = ---
+ *  2100    $dstHeight 
+ *  
+ * 
+ *  $dstWidth = 200;
+ *  $dstHeight = 2100*200/2800
+ *  $dstHeight = $srcHeight * $dstWidth / $srcWidth;
+ *  $dstHeight = 2100 * 200 / 1000
+ */
+
+    $dstWidth = 200;
+    $dstHeight = intval($srcHeight * $dstWidth / $srcWidth);
+
+    $pInfo = pathinfo ($src);
+    $dstFilename = $pInfo['filename'];
+    $dstExtension = $pInfo['extension'];
+    
+    $dstPath = './dest/' . $dstFilename . '_' . $dstWidth . 'x' . $dstHeight . '.' . $dstExtension;
+    $dstPathRetina = './dest/' . $dstFilename . '_' . $dstWidth . 'x' . $dstHeight . '@2x' . '.' . $dstExtension;
+    
+    
+    $dstImg = imagecreatetruecolor($dstWidth, $dstHeight);
+    imagecopyresampled($dstImg, $srcImg, 0, 0, 0, 0, $dstWidth, $dstHeight, $srcWidth, $srcHeight);
+    imagejpeg($dstImg, $dstPath);
+    
+    $dstImgRetina = imagecreatetruecolor($dstWidth * 2, $dstHeight * 2);
+    imagecopyresampled($dstImgRetina, $srcImg, 0, 0, 0, 0, $dstWidth*2, $dstHeight*2, $srcWidth, $srcHeight);
+    imagejpeg($dstImgRetina, $dstPathRetina);
+    
+?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>PHP - 01 Basic</title>
+        <title>PHP - 05 GD</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
         <link href="assets/css/styles.css" rel="stylesheet" type="text/css"/>
@@ -11,9 +55,16 @@
         <script src="assets/js/main.js" type="text/javascript"></script>
     </head>
     <body>
-        <h1>Bootstrap</h1>
+        <h1>PHP - GD Image Manipulation</h1>
         <div class="container">
-            
+            <img class="rounded" src="<?php echo $dstImg ?>">
         </div>
+
+        <hr>
+        <pre>
+            <?php
+//            var_dump($pInfo);
+            ?>
+        </pre>
     </body>
 </html>
